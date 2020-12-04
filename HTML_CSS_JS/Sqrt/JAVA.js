74eb92e471119d2ce539ebcdfff5fe49a155a682
Rodion Grinberg <42215667+rbeat@users.noreply.github.com>
2020-12-04 12:13:29 +0200

    Add files via upload

---
+++ HTML_CSS_JS/Sqrt/JAVA.js
@@ -0,0 +1,8 @@
+var a = Number(prompt("Enter number a"));
+var b = Number(prompt("Enter number b"));
+var sum = 0;
+for(var i=a;i<=b;i++){
+sum=sum+i*i
+}
+var result=sum;
+alert(result);
---
+++ HTML_CSS_JS/SummingNums/i.js
@@ -0,0 +1,7 @@
+var Number;
+var sum = 0;
+for(var i = 1; i <= 10; i++){
+number = Number(prompt("Enter number:" + i));
+sum = sum + number
+}
+alert(sum);    
